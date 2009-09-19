1c779d547448f23bbfa29ccbbbb97e9b94652e0f
Henk Punt <henkpunt@gmail.com>
2009-09-19 18:52:45 +0200

    initial memcached work

--- lib/concurrence/memcache/client.py
+++ lib/concurrence/memcache/client.py
@@ -9,11 +9,12 @@ from concurrence.io.socket import Socket
 from concurrence.io.buffered import BufferedStream
 from concurrence.containers.deque import Deque
 
+import logging
 import cPickle as pickle
 
-#TODO async set
+#TODO:
 #proper buffer sizes
-#bundling of multiple requests in 1 flush
+#bundling of multiple requests in 1 flush (autoflush on/off)
 #statistics
 #not use pickle for string and unicode types (use flags to indicate this)
 #timeout on commands (for clients, support Timeout.current)
@@ -21,78 +22,154 @@ import cPickle as pickle
 #todo detect timeouts on write/read, and mark host as dead
 #keep some time before retrying host
 #global consistent hashing algorithm for accessing set of memcached servers
-
-#rename MemcacheNode to MemcacheConnection.
-#first, there will be max 1 connection to each host in the system
+#replicated set
+#multiple connections to same host
+#append, prepend commands
+#close down node no recv ERROR?
+#support for cas command
+#UPD support
+#binary support
 
 class MemcacheError(Exception):
     pass
 
-class MemcacheNode(object):
+class MemcacheResult(object):
+    pass
+        
+MemcacheResult.STORED = MemcacheResult()
+MemcacheResult.NOT_STORED = MemcacheResult()
+MemcacheResult.EXISTS = MemcacheResult()
+MemcacheResult.NOT_FOUND = MemcacheResult()
+MemcacheResult.DELETED = MemcacheResult()
+
+MemcacheResult.ALL = {"STORED": MemcacheResult.STORED,
+                      "NOT_STORED": MemcacheResult.NOT_STORED,
+                      "EXISTS": MemcacheResult.EXISTS,
+                      "NOT_FOUND": MemcacheResult.NOT_FOUND,
+                      "DELETED": MemcacheResult.DELETED}
+
+class MemcacheCommand(object):
+    def __init__(self):
+        super(MemcacheCommand, self).__init__()
+        self.channel = Channel()
+
+class MemcacheCommandGet(MemcacheCommand):
+    def __init__(self, keys):
+        super(MemcacheCommandGet, self).__init__()
+        self.keys = keys
+        self.result = {}
+
+    def write(self, writer):
+        writer.write_bytes("get")
+        for key in self.keys:
+            writer.write_bytes(" " + key)
+
+    def read(self, reader):
+        result = {}
+        while True:
+            response_line = reader.read_line()
+            if response_line.startswith('VALUE'):
+                response_fields = response_line.split(' ')
+                key = response_fields[1]
+                flags = int(response_fields[2])
+                n = int(response_fields[3])
+                encoded_value = reader.read_bytes(n)
+                reader.read_line() #\r\n
+                result[key] = pickle.loads(encoded_value)
+            elif response_line == 'END':
+                return result 
+            else:
+                assert False, "protocol error"
+
+class MemcacheCommandWithResult(MemcacheCommand):
+    def read(self, reader):
+        response_line = reader.read_line()
+        #print response_line
+        result = MemcacheResult.ALL.get(response_line, None)
+        assert result is not None, "protocol error"
+        return result
+    
+class MemcacheCommandDelete(MemcacheCommandWithResult):
+    def __init__(self, key):
+        super(MemcacheCommandDelete, self).__init__()
+        self.key = key
+
+    def write(self, writer):
+        writer.write_bytes("delete %s" % (self.key, ))
+
+class MemcacheCommandStorage(MemcacheCommandWithResult):
+    def __init__(self, key, value, flags):
+        super(MemcacheCommandStorage, self).__init__()
+        self.key = key
+        self.value = value
+        self.flags = flags
+
+    def write(self, writer):
+        encoded_value = pickle.dumps(self.value, -1)
+        writer.write_bytes("%s %s %d 0 %d\r\n" % (self.cmd, self.key, self.flags, len(encoded_value)))
+        writer.write_bytes(encoded_value)
+
+class MemcacheCommandSet(MemcacheCommandStorage):
+    cmd = "set"
+
+class MemcacheCommandAdd(MemcacheCommandStorage):
+    cmd = "add"
+
+class MemcacheCommandReplace(MemcacheCommandStorage):
+    cmd = "replace"
+
+class MemcacheConnection(object):
     """this represents the connection/protocol to 1 memcached host
     this class supports concurrent usage of get/set methods by multiple
     tasks, the cmds are queued and performed in order agains the memcached host.    
     """
+    log = logging.getLogger('MemcacheConnection')
+
     def __init__(self):
         self._stream = None
 
     def connect(self, addr):
-        assert self._stream is None, "must not be disconneted before connecting"
+        assert self._stream is None, "already connected"
         self._stream = BufferedStream(Socket.connect(addr, Timeout.current()))
         self._command_queue = Deque()
         self._response_queue = Deque()
         self._command_writer_task = Tasklet.new(self._command_writer)()
         self._response_reader_task = Tasklet.new(self._response_reader)()
 
+    def close(self):
+        self._response_reader_task.kill()
+        self._command_writer_task.kill()
+        self._response_reader_task = None
+        self._command_writer_task = None
+        #raise exception on all waiting tasks still in the queues
+        for cmd in self._command_queue:
+            cmd.channel.send_exception(TaskletError, e, Tasklet.current())
+        for cmd in self._response_queue:
+            cmd.channel.send_exception(TaskletError, e, Tasklet.current())
+        self._command_queue = None
+        self._response_queue = None
+        self._stream.close()
+        self._stream = None
+
     def _read_response(self, reader):
-        cmd, block_channel = self._response_queue.popleft(True)
+        cmd = self._response_queue.popleft(True)
         try:
-            if cmd == 'get':
-                result = {} #we will gather 'get' results here
-            else:
-                result = True
-            while True:
-                response_line = reader.read_line()
-                if cmd == 'get' and response_line.startswith('VALUE'):
-                    response_fields = response_line.split(' ')
-                    key = response_fields[1]
-                    flags = int(response_fields[2])
-                    n = int(response_fields[3])
-                    encoded_value = reader.read_bytes(n)
-                    reader.read_line() #\r\n
-                    result[key] = pickle.loads(encoded_value)
-                elif cmd == 'get' and response_line == 'END':
-                    block_channel.send(result)
-                    break                            
-                elif cmd == 'set' and response_line == 'STORED':
-                    block_channel.send(result)
-                    break        
-                else:
-                    assert False, "unknown protocol state, cmd: %s, response_line: %s" % (cmd, response_line)
+            result = cmd.read(reader)
+            cmd.channel.send(result)
         except Exception, e:
-            block_channel.send_exception(TaskletError, e, Tasklet.current())
+            cmd.channel.send_exception(TaskletError, e, Tasklet.current())
             raise 
 
     def _write_command(self, writer):
         try:
-            cmd, args, block_channel = self._command_queue.popleft(True)
+            cmd = self._command_queue.popleft(True)
             writer.clear()
-            if cmd == 'get':
-                writer.write_bytes("get")
-                for key in args[0]:
-                    writer.write_bytes(" " + key)
-            elif cmd in ['set']:
-                key, value, flags = args           
-                encoded_value = pickle.dumps(value, -1)
-                writer.write_bytes("%s %s %d 0 %d\r\n" % (cmd, key, flags, len(encoded_value)))
-                writer.write_bytes(encoded_value)
-            else:
-                assert False, "unknown command %s" % cmd
+            cmd.write(writer)
             writer.write_bytes('\r\n')
             writer.flush()
-            self._response_queue.append((cmd, block_channel))
+            self._response_queue.append(cmd)
         except Exception, e:
-            block_channel.send_exception(TaskletError, e, Tasklet.current())
+            cmd.channel.send_exception(TaskletError, e, Tasklet.current())
             raise
 
     def _response_reader(self):
@@ -101,7 +178,8 @@ class MemcacheNode(object):
             try:
                 self._read_response(reader)
             except Exception, e:
-                self.close(e, False, True)
+                self.close()
+                self.log.exception('error while reading response')
                 return #this ends reader
             
     def _command_writer(self):
@@ -110,49 +188,38 @@ class MemcacheNode(object):
             try:
                 self._write_command(writer)
             except Exception, e:
-                self.close(e, True, False)
+                self.close()
+                self.log.exception('error while writing command')
                 return #this ends writer
 
-    def _do_command(self, cmd, *args):
-        block_channel = Channel()
-        self._command_queue.append((cmd, args, block_channel))
+    def _do_command(self, cmd):
+        self._command_queue.append(cmd)
         try:
-            return block_channel.receive()
+            return cmd.channel.receive()
         except TaskletError, e:
             raise MemcacheError(str(e.cause))
 
-    def close(self, exception = None, kill_reader = True, kill_writer = True):
-        #assert False, reason
-        if kill_reader:     
-            self._response_reader_task.kill()
-        if kill_writer:
-            self._command_writer_task.kill()
-        self._response_reader_task = None
-        self._command_writer_task = None
-        #raise exception on all waiting tasks still in the queues
-        for cmd, args, block_channel in self._command_queue:
-            block_channel.send_exception(TaskletError, e, Tasklet.current())
-        for cmd, block_channel in self._response_queue:
-            block_channel.send_exception(TaskletError, e, Tasklet.current())
-        self._command_queue = None
-        self._response_queue = None
-        self._stream.close()
-        self._stream = None
-        
     def set(self, key, data, flags = 0):
-        self._do_command("set", key, data, flags)
+        return self._do_command(MemcacheCommandSet(key, data, flags))
 
-    def _get_one(self, key):
-        result = self._do_command("get", [key])
+    def add(self, key, data, flags = 0):
+        return self._do_command(MemcacheCommandAdd(key, data, flags))
+
+    def replace(self, key, data, flags = 0):
+        return self._do_command(MemcacheCommandReplace(key, data, flags))
+
+    def delete(self, key):
+        return self._do_command(MemcacheCommandDelete(key))
+
+    def get(self, key):
+        result = self._do_command(MemcacheCommandGet([key]))
         if key in result:
             return result[key]
         else:
             return None
 
-    def get(self, keys):
-        if type(keys) == str:
-            return self._get_one(keys)
-        else:
-            return self._do_command("get", keys)
+    def multi_get(self, keys):
+        return self._do_command(MemcacheCommandGet(keys))
+
             
 
--- test/testmemcache.py
+++ test/testmemcache.py
@@ -1,25 +1,27 @@
 import os
 
 from concurrence import unittest, Tasklet
-from concurrence.memcache.client import MemcacheNode, MemcacheError
+from concurrence.memcache.client import MemcacheConnection, MemcacheError, MemcacheResult
 
 class MemcacheTest(unittest.TestCase):
     def testNodeBasic(self):
         
-        node = MemcacheNode()
+        node = MemcacheConnection()
         node.connect(('127.0.0.1', 11211))
 
-        node.set('test1', '12345')
-        node.set('test2', '67890')
+        self.assertEquals(MemcacheResult.STORED, node.set('test1', '12345'))
+        self.assertEquals(MemcacheResult.STORED, node.set('test2', '67890'))
 
         self.assertEquals('12345', node.get('test1'))
+        self.assertEquals('67890', node.get('test2'))
+
         self.assertEquals(None, node.get('test3'))
-        self.assertEquals({'test1': '12345', 'test2': '67890'}, node.get(['test1', 'test2', 'test3']))
+        self.assertEquals({'test1': '12345', 'test2': '67890'}, node.multi_get(['test1', 'test2', 'test3']))
 
         #update test2
         node.set('test2', 'hello world!')
 
-        self.assertEquals({'test1': '12345', 'test2': 'hello world!'}, node.get(['test1', 'test2', 'test3']))
+        self.assertEquals({'test1': '12345', 'test2': 'hello world!'}, node.multi_get(['test1', 'test2', 'test3']))
        
         #update to unicode type
         node.set('test2', u'C\xe9line')
@@ -28,6 +30,25 @@ class MemcacheTest(unittest.TestCase):
         #update to some other type
         node.set('test2', {'piet': 'blaat', 10: 20})
         self.assertEquals({'piet': 'blaat', 10: 20}, node.get('test2'))
+
+        #test delete
+        self.assertEquals(MemcacheResult.NOT_FOUND, node.delete('test_del1'))
+        self.assertEquals(MemcacheResult.STORED, node.set('test_del1', 'hello'))
+        self.assertEquals('hello', node.get('test_del1'))
+        self.assertEquals(MemcacheResult.DELETED, node.delete('test_del1'))
+        self.assertEquals(None, node.get('test_del1'))
+ 
+        #test add command
+        node.delete('add1')
+        self.assertEquals(MemcacheResult.STORED, node.add('add1', '11111'))
+        self.assertEquals(MemcacheResult.NOT_STORED, node.add('add1', '22222'))
+
+        #test replace
+        self.assertEquals(MemcacheResult.STORED, node.set('replace1', '11111'))
+        self.assertEquals(MemcacheResult.STORED, node.replace('replace1', '11111'))
+        self.assertEquals(MemcacheResult.STORED, node.replace('replace1', '11111'))
+        self.assertEquals(MemcacheResult.DELETED, node.delete('replace1'))
+        self.assertEquals(MemcacheResult.NOT_STORED, node.replace('replace1', '11111'))
 
         node.close()
         
